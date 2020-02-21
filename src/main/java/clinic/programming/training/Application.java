320624790e66b05f325dd985b9b76094f277089e
Rashid-Hussain-CSE <rashidhussain.cse@gmail.com>
2020-02-21 22:38:34 +0530

    Application java source

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,14 @@
+package clinic.programming.training;
+
+public class Application {
+    
+    public Application() {
+        System.out.println ("This is maven Project");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Hello World");
+	Application app = new Application();
+    }
+}
